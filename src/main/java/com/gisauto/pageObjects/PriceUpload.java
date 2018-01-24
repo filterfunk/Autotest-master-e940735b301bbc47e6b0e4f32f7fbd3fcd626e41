db6ce696cffa357243593476533297988eab0d06
artem.neradko <neradk1996@mail.ru>
2018-01-24 15:14:58 +0700

    stepdefs refactor, added new case

--- src/main/java/com/gisauto/pageObjects/PriceUpload.java
+++ src/main/java/com/gisauto/pageObjects/PriceUpload.java
@@ -1,7 +1,106 @@
 package com.gisauto.pageObjects;
 
+import com.gisauto.utils.FileUploader;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 public class PriceUpload extends BasePage {
 
-    //TODO: описать страницу "Загрузка прайсов"
+    private By handUploadButton = new By.ByXPath("//*[@id=\"mainWin\"]/div[2]/div[1]/div/button[3]"),
+            fileUpload = new By.ByXPath("//*[@id=\"parts-hand\"]/div/form/div[1]/div[1]/div/div[1]"),
+            shopSelect = new By.ByXPath("//*[@id=\"parts-hand\"]/div/form/div[2]/div/div[1]/div[1]/input"),
+            firstLinePlus = new By.ByXPath("//*[@id=\"item_import_template_firstLine-styler\"]/div[3]"),
+            deviderSelect = new By.ByXPath("//*[@id=\"parts-hand\"]/div/form/div[3]/div[3]/div[2]/div/div[1]/div[1]/input"),
+            firstDevider = new By.ByXPath("//*[@id=\"parts-hand\"]/div/form/div[3]/div[3]/div[2]/div/div[1]/div[2]/ul/li[1]"),
+            secondDevider = new By.ByXPath("//*[@id=\"parts-hand\"]/div/form/div[3]/div[3]/div[2]/div/div[1]/div[2]/ul/li[2]"),
+            thirdDevider = new By.ByXPath("//*[@id=\"parts-hand\"]/div/form/div[3]/div[3]/div[2]/div/div[1]/div[2]/ul/li[3]"),
+            templateSelect = new By.ByXPath("//*[@id=\"parts-hand\"]/div/form/div[4]/div[1]/div/div[1]/input"),
+            firstTemplate = new By.ByXPath("//*[@id=\"parts-hand\"]/div/form/div[4]/div[1]/div/div[2]/ul/li[1]"),
+            createTemplateButton = new By.ByXPath("//*[@id=\"parts-hand\"]/div/form/div[4]/div[1]/button"),
+            currencySelect = new By.ByXPath("//*[@id=\"parts-hand\"]/div/form/div[4]/div[2]/div[1]/div[1]/div[1]/input"),
+            saveButton = new By.ByXPath("//*[@id=\"parts-hand\"]/div/form/button");
+
+    public PriceUpload clickOnHandUploadButton() {
+        getElement(handUploadButton).click();
+        return this;
+    }
+
+    public PriceUpload clickOnFileUpload(String file) {
+        getElement(fileUpload).click();
+        FileUploader.uploadFile(file);
+        return this;
+    }
+
+    public PriceUpload clickOnShopSelect() {
+        getElement(shopSelect).click();
+        return this;
+    }
+
+    public PriceUpload clickOnShop(String shopName) {
+        getShopFromSelector(shopName).click();
+        return this;
+    }
+
+    public PriceUpload clickOnFirstLinePlus(int times) {
+        for (int i = 0; i < times; i++)
+            getElement(firstLinePlus).click();
+        return this;
+    }
+
+    public PriceUpload clickOnDeviderSelect() {
+        getElement(deviderSelect).click();
+        return this;
+    }
+
+    public PriceUpload clickOnFirstDevider() {
+        getElement(firstDevider).click();
+        return this;
+    }
+
+    public PriceUpload clickOnSecondDevider() {
+        getElement(secondDevider).click();
+        return this;
+    }
+
+    public PriceUpload clickOnThirdDevider() {
+        getElement(thirdDevider).click();
+        return this;
+    }
+
+    public PriceUpload clickOnTemplateSelect() {
+        getElement(templateSelect).click();
+        return this;
+    }
+
+    public PriceUpload clickOnFirstTemplate() {
+        getElement(firstTemplate).click();
+        return this;
+    }
+
+    public PriceUpload clickOnCreateTemplateButton() {
+        getElement(createTemplateButton).click();
+        return this;
+    }
+
+    public PriceUpload clickOnCurrencySelect() {
+        getElement(currencySelect).click();
+        return this;
+    }
+
+    public PriceUpload clickOnSaveButton() {
+        getElement(saveButton).click();
+        return this;
+    }
+
+    private WebElement getShopFromSelector(String shopName) {
+        int i = 0;
+        WebElement element = null;
+        do {
+            i++;
+            element = getElement(new By.ByXPath("\"//*[@id=\\\"parts-hand\\\"]/div/form/div[2]/div/div[1]/div[2]/ul/li[" + i + "]\""));
+        }
+        while (element.getText().equals(shopName));
+        return element;
+    }
 
 }
--- src/test/java/com/gisauto/hooks/Hooks.java
+++ src/test/java/com/gisauto/hooks/Hooks.java
@@ -13,30 +13,6 @@ import java.io.IOException;
 
 public final class Hooks {
 
-    /**
-     * Делает скриншот и прикрепляет к отчёту Yandex.allure
-     *
-     * @return массив байт с изображением
-     */
-    @Attachment
-    public static byte[] screenShot() {
-        byte[] out = null;
-
-        try {
-            BufferedImage screenShot = new Robot().createScreenCapture(
-                    new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
-            ByteArrayOutputStream bo = new ByteArrayOutputStream();
-            ImageIO.write(screenShot, "png", bo);
-            out = bo.toByteArray();
-            bo.close();
-        } catch (AWTException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return out;
-    }
-
     @After
     public void quit() {
 //        Assert.assertEquals(false, PF.getPage(HomePage.class).isLoggedIn());

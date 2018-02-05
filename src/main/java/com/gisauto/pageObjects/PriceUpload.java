74d7f7393feca8fc06c31b1228a7f39670816516
artem.neradko <neradk1996@mail.ru>
2018-02-05 12:00:18 +0700

    refactor, added methods for auto upload

--- src/main/java/com/gisauto/pageObjects/PriceUpload.java
+++ src/main/java/com/gisauto/pageObjects/PriceUpload.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebElement;
 
 public class PriceUpload extends BasePage {
 
-    private By handUploadButton = new By.ByXPath("//*[@id=\"mainWin\"]/div[2]/div[1]/div/button[3]"),
+    private By manualUploadButton = new By.ByXPath("//*[@id=\"mainWin\"]/div[2]/div[1]/div/button[3]"),
             fileUpload = new By.ByXPath("//*[@id=\"parts-hand\"]/div/form/div[1]/div[1]/div/div[1]"),
             shopSelect = new By.ByXPath("//*[@id=\"parts-hand\"]/div/form/div[2]/div/div[1]/div[1]/input"),
             firstLinePlus = new By.ByXPath("//*[@id=\"item_import_template_firstLine-styler\"]/div[3]"),
@@ -20,10 +20,48 @@ public class PriceUpload extends BasePage {
             createTemplateButton = new By.ByXPath("//*[@id=\"parts-hand\"]/div/form/div[4]/div[1]/button"),
             currencySelect = new By.ByXPath("//*[@id=\"parts-hand\"]/div/form/div[4]/div[2]/div[1]/div[1]/div[1]/input"),
             saveButton = new By.ByXPath("//*[@id=\"parts-hand\"]/div/form/button"),
-            templateSettings = new By.ByXPath("//*[@id=\"mainWin\"]/div[2]/div[1]/div/button[2]");
+            templateSettings = new By.ByXPath("//*[@id=\"mainWin\"]/div[2]/div[1]/div/button[2]"),
+            createUploadButton = new By.ByXPath("//*[@id=\"mainWin\"]/div[2]/div[1]/div/button[1]"),
+            uploadByEmail = new By.ByXPath("//*[@id=\"item-autoload-form\"]/div[2]/div[2]/ul/li[1]/a/button"),
+            uploadByFTP = new By.ByXPath("//*[@id=\"item-autoload-form\"]/div[2]/div[2]/ul/li[3]/a/button"),
+            uploadByLink = new By.ByXPath("//*[@id=\"item-autoload-form\"]/div[2]/div[2]/ul/li[4]/a/button"),
+            fileNameInput = new By.ByXPath("//*[@id=\"item_autoload_emailFileName\"]"),
+            email = new By.ByXPath("//*[@id=\"item_autoload_email\"]"),
+            shopMultiSelect = new By.ByXPath("//*[@id=\"item-autoload-form\"]/div[6]/div[1]/div/div/div[1]/div[2]"),
+            templateMultiSelect = new By.ByXPath("//*[@id=\"item-autoload-form\"]/div[6]/div[2]/div/div/div[1]/div[2]");
 
-    public PriceUpload clickOnHandUploadButton() {
-        getElement(handUploadButton).click();
+    public PriceUpload clickOnCreateUploadButton() {
+        getElement(createUploadButton).click();
+        return this;
+    }
+
+    public PriceUpload clickOnUploadByEmail() {
+        getElement(uploadByEmail).click();
+        return this;
+    }
+
+    public PriceUpload clickOnUploadByFTP() {
+        getElement(uploadByFTP).click();
+        return this;
+    }
+
+    public PriceUpload clickOnUploadByLink() {
+        getElement(uploadByLink).click();
+        return this;
+    }
+
+    public PriceUpload inputFileName(String text) {
+        getElement(fileNameInput).sendKeys(text);
+        return this;
+    }
+
+    public PriceUpload inputEmail(String text) {
+        getElement(email).sendKeys(text);
+        return this;
+    }
+
+    public PriceUpload clickOnManualUploadButton() {
+        getElement(manualUploadButton).click();
         return this;
     }
 
@@ -108,6 +146,22 @@ public class PriceUpload extends BasePage {
     public TemplateSettings clickOnTemplateSettings() {
         getElement(templateSettings).click();
         return PF.getPage(TemplateSettings.class);
+    }
+
+    public PriceUpload clickOnShopFromSelect(String shopName){
+        getShopFromMultiSelect(shopName).click();
+        return this;
+    }
+
+    private WebElement getShopFromMultiSelect(String shopName) {
+        int i = 0;
+        WebElement element = null;
+        do {
+            i++;
+            element = getElement(new By.ByXPath("//*[@id=\"item-autoload-form\"]/div[6]/div[1]/div/div/div[2]/ul/li[" + i + "]/label"));
+        }
+        while (element.getText().equals(shopName));
+        return element;
     }
 
 }

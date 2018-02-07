25b18c7d6295cf97766835a52dbf3706ee8271f1
artem.neradko <neradk1996@mail.ru>
2018-02-07 11:07:34 +0700

    refactor

--- src/test/java/com/gisauto/hooks/Hooks.java
+++ src/test/java/com/gisauto/hooks/Hooks.java
@@ -2,9 +2,11 @@ package com.gisauto.hooks;
 
 import com.gisauto.utils.Driver;
 import com.gisauto.utils.PF;
+import com.gisauto.utils.UF;
 import cucumber.api.java.After;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import static java.lang.System.*;
 
 
 @RunWith(Suite.class)
@@ -14,6 +16,8 @@ public final class Hooks {
     public void quit() {
 //        Assert.assertEquals(false, PF.getPage(HomePage.class).isLoggedIn());
         PF.reset();
+        UF.reset();
+        gc();
         Driver.kill();
     }
 

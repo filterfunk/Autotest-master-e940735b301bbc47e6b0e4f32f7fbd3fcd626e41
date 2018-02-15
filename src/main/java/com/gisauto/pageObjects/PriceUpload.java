32de55fac10cc51a9152d11b015402fdf6112f22
artem.neradko <neradk1996@mail.ru>
2018-02-15 15:15:55 +0700

    added new class autoupload and stepdefs for autoupload

--- src/main/java/com/gisauto/pageObjects/PriceUpload.java
+++ src/main/java/com/gisauto/pageObjects/PriceUpload.java
@@ -20,44 +20,12 @@ public class PriceUpload extends BasePage {
             createTemplateButton = new By.ByXPath("//*[@id=\"parts-hand\"]/div/form/div[4]/div[1]/button"),
             currencySelect = new By.ByXPath("//*[@id=\"parts-hand\"]/div/form/div[4]/div[2]/div[1]/div[1]/div[1]/input"),
             saveButton = new By.ByXPath("//*[@id=\"parts-hand\"]/div/form/button"),
-            templateSettings = new By.ByXPath("//*[@id=\"mainWin\"]/div[2]/div[1]/div/button[2]"),
-            createUploadButton = new By.ByXPath("//*[@id=\"mainWin\"]/div[2]/div[1]/div/button[1]"),
-            uploadByEmail = new By.ByXPath("//*[@id=\"item-autoload-form\"]/div[2]/div[2]/ul/li[1]/a/button"),
-            uploadByFTP = new By.ByXPath("//*[@id=\"item-autoload-form\"]/div[2]/div[2]/ul/li[3]/a/button"),
-            uploadByLink = new By.ByXPath("//*[@id=\"item-autoload-form\"]/div[2]/div[2]/ul/li[4]/a/button"),
-            fileNameInput = new By.ByXPath("//*[@id=\"item_autoload_emailFileName\"]"),
-            email = new By.ByXPath("//*[@id=\"item_autoload_email\"]"),
-            shopMultiSelect = new By.ByXPath("//*[@id=\"item-autoload-form\"]/div[6]/div[1]/div/div/div[1]/div[2]"),
-            templateMultiSelect = new By.ByXPath("//*[@id=\"item-autoload-form\"]/div[6]/div[2]/div/div/div[1]/div[2]");
+            createUpolad = new By.ByXPath("//*[@id=\"mainWin\"]/div[2]/div[1]/div/button[1]"),
+            templateSettings = new By.ByXPath("//*[@id=\"mainWin\"]/div[2]/div[1]/div/button[2]");
 
-    public PriceUpload clickOnCreateUploadButton() {
-        getElement(createUploadButton).click();
-        return this;
-    }
-
-    public PriceUpload clickOnUploadByEmail() {
-        getElement(uploadByEmail).click();
-        return this;
-    }
-
-    public PriceUpload clickOnUploadByFTP() {
-        getElement(uploadByFTP).click();
-        return this;
-    }
-
-    public PriceUpload clickOnUploadByLink() {
-        getElement(uploadByLink).click();
-        return this;
-    }
-
-    public PriceUpload inputFileName(String text) {
-        getElement(fileNameInput).sendKeys(text);
-        return this;
-    }
-
-    public PriceUpload inputEmail(String text) {
-        getElement(email).sendKeys(text);
-        return this;
+    public AutoUpload clickOnCreateUpload() {
+        getElement(createUpolad).click();
+        return PF.getPage(AutoUpload.class);
     }
 
     public PriceUpload clickOnManualUploadButton() {
@@ -148,20 +116,112 @@ public class PriceUpload extends BasePage {
         return PF.getPage(TemplateSettings.class);
     }
 
-    public PriceUpload clickOnShopFromSelect(String shopName){
-        getShopFromMultiSelect(shopName).click();
-        return this;
-    }
+    public class AutoUpload extends PriceUpload {
 
-    private WebElement getShopFromMultiSelect(String shopName) {
-        int i = 0;
-        WebElement element = null;
-        do {
-            i++;
-            element = getElement(new By.ByXPath("//*[@id=\"item-autoload-form\"]/div[6]/div[1]/div/div/div[2]/ul/li[" + i + "]/label"));
+        private final By emailUploadButton = new By.ByXPath("//*[@id=\"item-autoload-form\"]/div[2]/div[2]/ul/li[1]/a/button"),
+                ftpUploadButton = new By.ByXPath("//*[@id=\"item-autoload-form\"]/div[2]/div[2]/ul/li[3]/a/button"),
+                directLinkUploadButton = new By.ByXPath("//*[@id=\"item-autoload-form\"]/div[2]/div[2]/ul/li[4]/a/button"),
+                emailFileNameInput = new By.ByXPath("//*[@id=\"item_autoload_emailFileName\"]"),
+                ftpFileNameInput = new By.ByXPath("//*[@id=\"item_autoload_ftpFileName\"]"),
+                emailInput = new By.ByXPath("//*[@id=\"item_autoload_email\"]"),
+                shopSelect = new By.ByXPath("//*[@id=\"item-autoload-form\"]/div[6]/div[1]/div/div/div[1]/div[2]"),
+                templateSelect = new By.ByXPath("//*[@id=\"item-autoload-form\"]/div[6]/div[2]/div/div/div[1]/div[2]"),
+                linkAdressInput = new By.ByXPath("//*[@id=\"item_autoload_sourceLink\"]"),
+                createButton = new By.ByXPath("//*[@id=\"save-autoload-button\"]");
+
+        public AutoUpload typeEmailFileName(String fileName) {
+            inputText(getElement(emailFileNameInput), fileName);
+            return this;
         }
-        while (element.getText().equals(shopName));
-        return element;
+
+        public AutoUpload typeFTPFileName(String fileName) {
+            inputText(getElement(ftpFileNameInput), fileName);
+            return this;
+        }
+
+        public AutoUpload typeAutoUploadEmail(String email) {
+            inputText(getElement(emailInput), email);
+            return this;
+        }
+
+        public AutoUpload clickOnRefreshRateSelect(boolean isFTP, String refreshRate) {
+            String id = isFTP ? "up-by-ftp" : "up-by-link";
+            getElement(new By.ByXPath("//*[@id=\"" + id + "\"]/div[2]/div/div/div[1]/input")).click();
+            getRefreshRate(id, refreshRate).click();
+            return this;
+        }
+
+        private WebElement getRefreshRate(String id, String rate) {
+            int li = 0;
+
+            do {
+                li++;
+                WebElement a = getElement(new By.ByXPath("//*[@id=\"" + id + "\"]/div[2]/div/div/div[2]/ul/li[" + li + "]/label/span"));
+                if (a.getText().equals(rate)) return a;
+            } while (li <= 3);
+
+            throw new RuntimeException("Невозможно найти частоту обновления " + rate);
+        }
+
+        public AutoUpload clickOnShopSelect(String shopName) {
+            getElement(shopSelect).click();
+            getShopFromSelect(shopName).click();
+            return this;
+        }
+
+        private WebElement getShopFromSelect(String shopName) {
+            int li = 0;
+
+            do {
+                li++;
+                WebElement a = getElement(new By.ByXPath("//*[@id=\"item-autoload-form\"]/div[6]/div[1]/div/div/div[2]/ul/li[" + li + "]/label/span"));
+                if (a.getText().equals(shopName)) return a;
+            } while (li <= 15);
+
+            throw new RuntimeException("Невозможно найти магазин " + shopName);
+        }
+
+        public AutoUpload clickOnTemplateSelect(String templateName) {
+            getElement(templateSelect).click();
+            getTemplateFromSelect(templateName).click();
+            return this;
+        }
+
+        private WebElement getTemplateFromSelect(String templateName) {
+            int li = 0;
+
+            do {
+                li++;
+                WebElement a = getElement(new By.ByXPath("//*[@id=\"item-autoload-form\"]/div[6]/div[2]/div/div/div[2]/ul/li[" + li + "]/label/span"));
+                if (a.getText().equals(templateName)) return a;
+            } while (li <= 2);
+
+            throw new RuntimeException("Невозможно найти шаблон " + templateName);
+        }
+
+        public AutoUpload typeLinkAdress(String url) {
+            inputText(getElement(linkAdressInput), url);
+            return this;
+        }
+
+        public AutoUpload clickOnUploadType(String uploadType) {
+            if (uploadType.toLowerCase().equals("email")) {
+                getElement(emailUploadButton).click();
+            } else if (uploadType.toLowerCase().equals("ftp")) {
+                getElement(ftpUploadButton).click();
+            } else if (uploadType.toLowerCase().equals("link")) {
+                getElement(directLinkUploadButton).click();
+            } else {
+                throw new RuntimeException("Неизвестный тип загрузки - " + uploadType);
+            }
+            return this;
+        }
+
+        public AutoUpload clickOnCreateButton() {
+            getElement(createButton).click();
+            return this;
+        }
+
     }
 
 }
---
+++ src/test/java/com/gisauto/stepdefs/AutoUploadStepDefs.java
@@ -0,0 +1,64 @@
+package com.gisauto.stepdefs;
+
+import com.gisauto.pageObjects.PriceUpload;
+import com.gisauto.utils.PF;
+import cucumber.api.java.ru.Если;
+
+public class AutoUploadStepDefs {
+
+    @Если("^пользователь нажимает создать загрузку$")
+    public void пользовательНажимаетСоздатьЗагрузку() {
+        PF.getPage(PriceUpload.class).clickOnCreateUpload();
+    }
+
+    @Если("^пользователь выбирает тип загрузки \"([^\"]*)\"$")
+    public void пользовательВыбираетТипЗагрузки(String uploadType) {
+        PF.getPage(PriceUpload.AutoUpload.class).clickOnUploadType(uploadType);
+    }
+
+    @Если("^пользователь вводит имя файла \"([^\"]*)\" email$")
+    public void пользовательВводитИмяФайлаEmail(String fileName) {
+        PF.getPage(PriceUpload.AutoUpload.class).typeEmailFileName(fileName);
+    }
+
+    @Если("^пользователь вводит электронную почту \"([^\"]*)\" email$")
+    public void пользовательВводитЭлектроннуюПочтуEmail(String email) {
+        PF.getPage(PriceUpload.AutoUpload.class).typeAutoUploadEmail(email);
+    }
+
+    @Если("^пользователь вводит имя файла \"([^\"]*)\" ftp$")
+    public void пользовательВводитИмяФайлаFTP(String fileName) {
+        PF.getPage(PriceUpload.AutoUpload.class).typeFTPFileName(fileName);
+    }
+
+    @Если("^пользователь вводит адрес ссылки \"([^\"]*)\"$")
+    public void пользовательВводитАдресСсылки(String url) {
+        PF.getPage(PriceUpload.AutoUpload.class).typeLinkAdress(url);
+    }
+
+    @Если("^пользотвалье выбирает частоту обновления \"([^\"]*)\" для \"([^\"]*)\" типа загрузки^")
+    public void пользовательВыбраетЧастотуОбновления(String refreshRate, String uploadType) {
+        PF.getPage(PriceUpload.AutoUpload.class)
+                .clickOnRefreshRateSelect(
+                        uploadType
+                                .toLowerCase()
+                                .equals("ftp"),
+                        refreshRate);
+    }
+
+    @Если("^пользователь выбирает магазин \"([^\"]*)\"$")
+    public void пользовательВыбираетМагазин(String shopName) {
+        PF.getPage(PriceUpload.AutoUpload.class).clickOnShopSelect(shopName);
+    }
+
+    @Если("^пользователь выбирает шаблон \"([^\"]*)\"$")
+    public void пользовательВыбираетШаблон(String templateName) {
+        PF.getPage(PriceUpload.AutoUpload.class).clickOnTemplateSelect(templateName);
+    }
+
+    @Если("^пользователь нажимает создать")
+    public void пользовательНажимаетСоздать() {
+        PF.getPage(PriceUpload.AutoUpload.class).clickOnCreateButton();
+    }
+
+}

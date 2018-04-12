b0873e5dbfe97270f633e8e4a5e41491d80be826
denis <d.bystray@gmail.com>
2018-04-12 15:20:53 +0700

    ADD Button2, Button3

--- src/main/java/com/gisauto/pageObjects/VIN.java
+++ src/main/java/com/gisauto/pageObjects/VIN.java
@@ -6,7 +6,9 @@ public class VIN extends BasePage {
 
     private By vinInput = new By.ByXPath("//*[@id=\"customer_request_vin\"]"),
             checkVINButton = new By.ByXPath("//*[@id=\"vin-check\"]"),
-            nextButton = new By.ByXPath("//*a[@class=\"btn btn-next nav-btn\"]"),
+            next1Button = new By.ByXPath("//*[@id=\"step-1\"]/div[8]/a\"]"),
+            next2Button = new By.ByXPath("//*[@id=\"step-2\"]/a[2]"),
+            next3Button = new By.ByXPath("//*[@id=\"form-submit\"]"),
             partNameInput = new By.ByXPath("//*[@id=\"customer_request_customerRequestElements_0_partName\"]"),
             typeButton = new By.ByXPath("//*[@id=\"part-container/div/div[3]/div[1]/div[1]/input\"]"),
             nameInput = new By.ByXPath("//*[@id=\"customer_request_customerName\"]"),
@@ -24,6 +26,18 @@ public class VIN extends BasePage {
         return this;
     }
 
+    public VIN clickOnNext1Button() {
+        getElement(next1Button).click();
+        return this;
+
+    }
+
+    public VIN typePartName(String text) {
+        inputText(getElement(partNameInput), text);
+        return this;
+
+    }
+
     public VIN clickOnPartType(String text) {
         getElementFromSelect("//*[@id=\"part-container\"]/div/div[3]/div[1]/div[2]/ul/li[2]/label/span[text()=\"",
                 "\"]",
@@ -32,13 +46,9 @@ public class VIN extends BasePage {
         return this;
     }
 
-    public VIN clickOnNextButton() {
-        getElement(nextButton).click();
-        return this;
-}
 
-    public VIN typePartName(String text) {
-        inputText(getElement(partNameInput), text);
+    public VIN clickOnNext2Button() {
+        getElement(next2Button).click();
         return this;
 
     }
@@ -58,4 +68,11 @@ public class VIN extends BasePage {
         getElementFromSelect("//*[@id=\"step-3\"]/div/div[1]/div[2]/div/div[1]/div[2]/ul/li[3]/label[text()=\"", "\"]", text, getElement(cityInput)).click();
         return this;
     }
+
+    public VIN clickOnNext3Button() {
+        getElement(next3Button).click();
+        return this;
+
+    }
+
 }

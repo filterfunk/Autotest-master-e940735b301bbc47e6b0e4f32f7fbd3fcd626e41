d49247a15abc1f3684b0b1471a3f0668392d3d8f
artem.neradko <neradk1996@mail.ru>
2018-04-17 16:48:19 +0700

    GA-843

--- src/main/java/com/gisauto/pageObjects/IncomingRequest.java
+++ src/main/java/com/gisauto/pageObjects/IncomingRequest.java
@@ -1,7 +1,60 @@
 package com.gisauto.pageObjects;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
 public class IncomingRequest extends BasePage {
 
-    //TODO: описать страницу "Входящие запросы"
+    private By date = new By.ByXPath(""),
+            auto = new By.ByXPath(""),
+            part = new By.ByXPath(""),
+            customer = new By.ByXPath("");
+
+
+    public boolean checkDate() {
+        List<WebElement> list = getElements(date);
+        for (WebElement we : list) {
+            SimpleDateFormat formatter = new SimpleDateFormat("dd.mm.yy");
+            Date date = new Date();
+            try {
+                if (we.getText().equals(formatter.parseObject(String.valueOf(date)))) {
+                    return true;
+                }
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public boolean checkAuto(String text) {
+        List<WebElement> list = getElements(auto);
+        for (WebElement we : list) {
+            if (we.getText().equals(text)) return true;
+        }
+        return false;
+    }
+
+    public boolean checkPart(String text) {
+        List<WebElement> list = getElements(part);
+        for (WebElement we : list) {
+            if (we.getText().equals(text)) return true;
+        }
+        return false;
+    }
+
+    public boolean checkCustomer(String text) {
+        List<WebElement> list = getElements(customer);
+        for (WebElement we : list) {
+            if (we.getText().equals(text)) return true;
+        }
+        return false;
+    }
+
 
 }
--- src/test/java/com/gisauto/stepdefs/IncomingRequestsStepDefs.java
+++ src/test/java/com/gisauto/stepdefs/IncomingRequestsStepDefs.java
@@ -1,35 +1,35 @@
 package com.gisauto.stepdefs;
 
+import com.gisauto.pageObjects.IncomingRequest;
+import com.gisauto.utils.PF;
 import cucumber.api.PendingException;
 import cucumber.api.java.ru.И;
 import cucumber.api.java.ru.То;
+import org.junit.Assert;
 
 /**
  * @autor neradko, created on 17.04.18
  */
 
 public class IncomingRequestsStepDefs {
+
     @То("^в таблице входящих запросов появляется текущая дата$")
-    public void вТаблицеВходящихЗапросовПоявляетсяТекущаяДата() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void вТаблицеВходящихЗапросовПоявляетсяТекущаяДата() {
+        Assert.assertEquals("", true, PF.getPage(IncomingRequest.class).checkDate());
     }
 
     @И("^в таблице входящих запросов появляется покупатель \"([^\"]*)\"$")
-    public void вТаблицеВходящихЗапросовПоявляетсяПокупатель(String arg0) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void вТаблицеВходящихЗапросовПоявляетсяПокупатель(String customer) {
+        Assert.assertEquals("", true, PF.getPage(IncomingRequest.class).checkCustomer(customer));
     }
 
     @И("^в таблице входящих запросов появляется авто \"([^\"]*)\"$")
-    public void вТаблицеВходящихЗапросовПоявляетсяАвто(String arg0) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void вТаблицеВходящихЗапросовПоявляетсяАвто(String auto) {
+        Assert.assertEquals("", true, PF.getPage(IncomingRequest.class).checkAuto(auto));
     }
 
     @И("^в таблице входящих запросов появляется запчасть \"([^\"]*)\"$")
-    public void вТаблицеВходящихЗапросовПоявляетсяЗапчасть(String arg0) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void вТаблицеВходящихЗапросовПоявляетсяЗапчасть(String part) {
+        Assert.assertEquals("", true, PF.getPage(IncomingRequest.class).checkPart(part));
     }
 }

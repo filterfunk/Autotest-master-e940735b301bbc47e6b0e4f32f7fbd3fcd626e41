18b2e6bbf33488a7df3e1a6020dd9d1423fb0528
denis <d.bystray@gmail.com>
2018-04-18 17:16:35 +0700

    add new scenario and asserts

--- src/main/java/com/gisauto/pageObjects/OutgoingRequests.java
+++ src/main/java/com/gisauto/pageObjects/OutgoingRequests.java
@@ -13,8 +13,8 @@ public class OutgoingRequests extends BasePage {
 
     private final By lastRequest = new By.ByXPath("//*[@id=\"main-data\"]/div[1]/div[1]/div/div[2]/table[2]/tbody/tr[1]/td[6]/div/div[1]/a"),
             date = new By.ByXPath("//table//td[@data-name=\"created_at\"]"),
-            brand = new By.ByXPath("//table//td[@data-name=\"parts\"]//div"),
-            name = new By.ByXPath("//table//td[@data-name=\"parts\"]//a\"]");
+            brand = new By.ByXPath("//table//td[@data-name=\"car\"]//p"),
+            name = new By.ByXPath("//table//td[@data-name=\"parts\"]//p");
 
     public OutgoingRequests checkRequest(String partName) {
         Assert.assertEquals("Запрос на " + partName + "не найден",
@@ -22,13 +22,21 @@ public class OutgoingRequests extends BasePage {
         return this;
     }
 
+    //Don't use this method yet
+    @Deprecated
     public boolean checkDate() {
         List<WebElement> list = getElements(date);
+        SimpleDateFormat formatter = new SimpleDateFormat("DD.MM.YY");
+        Date date = new Date();
+        System.out.println(date);
+        try {
+            System.out.println(formatter.parse(date.toString()));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
         for (WebElement we : list) {
-            SimpleDateFormat formatter = new SimpleDateFormat("dd.mm.yy");
-            Date date = new Date();
             try {
-                if (we.getText().equals(formatter.parseObject(String.valueOf(date)))) {
+                if (we.getText().equals(formatter.parseObject(date.toString()))) {
                     return true;
                 }
             } catch (ParseException e) {

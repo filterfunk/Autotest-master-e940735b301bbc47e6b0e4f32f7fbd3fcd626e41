aaa399b3aff514abdefd316c5448ee7ab6ca87c7
denis <d.bystray@gmail.com>
2018-04-19 12:50:39 +0700

    final

--- src/test/java/com/gisauto/RunnerTest.java
+++ src/test/java/com/gisauto/RunnerTest.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/java/com/gisauto/featuresRegress", //путь к папке с .feature файлами
         glue = {"com.gisauto.stepdefs", "com.gisauto.hooks"},   //пакет, в котором находятся классы с реализацией шагов и «хуков»
-        tags = "@all",                                          //фильтр запускаемых тестов по тэгам
+        tags = "@10",                                          //фильтр запускаемых тестов по тэгам
         dryRun = false,                                         // если true, то сразу после запуска теста фреймворк проверяет, все ли шаги теста разработаны, если нет, то выдает предупреждение
         strict = false,                                         //если true, то при встрече неразработанного шага тест остановится с ошибкой
         snippets = SnippetType.UNDERSCORE                       //указывает в каком формате фреймворк будет предлагать шаблон для нереализованных шагов
